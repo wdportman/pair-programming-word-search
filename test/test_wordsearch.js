12754e37f3ab24b7d1469c7ffdf76fa8162ae14f
Will <wdportman@gmail.com>
2020-09-24 01:05:43 +0000

    update

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -96,4 +96,24 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("________________Vertical______________should return false if the input array is []", function() {
+    const result = wordSearch([], 'EFMH')
+
+    assert.isFalse(result);
+  });
+  it("________________Vertical______________should return false if the word is an empty string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
 });
